8664ba78fd80862723b93189cafe18a176d06184
Maxime521 <9842@holbertonstudents.com>
2025-01-16 14:11:57 +0100

    correction the code

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    print("{}".format(chr(i)), end="")
+for ascii_value in range(97, 123):
+    if not (ascii_value == 101 or ascii_value == 113):
+        print("{}".format(chr(ascii_value)), end="")
