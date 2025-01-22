88945463827dc7ee21860db7946009b52227db3f
Maxime521 <9842@holbertonstudents.com>
2025-01-22 18:14:34 +0100

    correction code

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -3,4 +3,4 @@ def multiply_by_2(a_dictionary):
     a_new_dictinary = {}
     for key in a_dictionary:
         a_new_dictinary[key] = a_dictionary[key] * 2
-        return a_new_dictinary
+    return a_new_dictinary
