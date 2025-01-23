5d01d5161dda4c9e342574e1ac1ea11f31531539
Maxime521 <9842@holbertonstudents.com>
2025-01-23 22:12:01 +0100

    correction

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not isinstance(roman_string, str) or roman_string is None:
-        return 0
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    total = 0
-    prev_value = 0
-
-    for char in roman_string:
-        current_value = roman_dict[char]
-        if current_value > prev_value:
-            total += current_value - 2 * prev_value
-        else:
-            total += current_value
-        prev_value = current_value
-
-    return total
+    res = 0
+    if roman_string and isinstance(roman_string, str):
+        rti = {'M': 1000, 'D': 500, 'C': 100, 'L': 50,
+               'X': 10, 'V': 5, 'I': 1}
+        for i in range(len(roman_string)):
+            if i > 0 and rti[roman_string[i]] > rti[roman_string[i - 1]]:
+                res += rti[roman_string[i]] - (rti[roman_string[i - 1]] * 2)
+            else:
+                res += rti[roman_string[i]]
+    return res
