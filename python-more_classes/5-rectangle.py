9ac468c2c1b53996826c21bcacbc71e5f185a4b7
Maxime521 <9842@holbertonstudents.com>
2025-01-28 22:25:47 +0100

    I just finished tasks 5, 6, 7 and I got help for tasks 6 and 7 + chmod

---
+++ python-more_classes/5-rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""Rectangle generation module for Python project"""
+
+
+class Rectangle:
+    """
+    class Rectangle that defines a rectangle.
+    Properties: width and height.
+    Area and Perimeter methods.
+    Print, str, repr and del methods.
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rect = ""
+        for i in range(self.__height):
+            rect += "#" * self.__width
+            rect += "\n" if i != self.__height - 1 else ""
+        return rect
+
+    def __repr__(self):
+        return f'Rectangle({self.__width}, {self.__height})'
+
+    def __del__(self):
+        print('Bye rectangle...')
