cb153e91da3b6e3a43d01457bb511be16b238204
Maxime521 <9842@holbertonstudents.com>
2025-02-03 21:03:05 +0100

    I put my previous code back because the other one was not good

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """
-This module contains a class Square that inherits from Rectangle
-(9-rectangle.py)
+This module definies a class BaseGeometry
 """
 
 
-class BaseGeometry:
+class BaseGeometry():
     """
-    Implementation integer_validator and area method
+    Creates a Square class that inherits from Rectangle with a private
+    attribute size, without getter or setter, validated as a positive integer
+    by integer_validator, and implements the area() method
     """
     def area(self):
         raise Exception("area() is not implemented")
@@ -15,21 +16,33 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
+
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
 
 
-class Square(BaseGeometry):
+class Rectangle(BaseGeometry):
     """
-    Class representing a square, inheriting from BaseGeometry
-    """
-    def __init__(self, size):
-        self.integer_validator("size", size)
-        self.__size = size
+    A class Rectangle that inherits from BaseGeometry
+        """
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
 
     def area(self):
-        return (self.__size * self.__size)
+        return self.__width * self.__height
 
-    @property
-    def size(self):
-        return self.__size
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+
+class Square(Rectangle):
+    """
+    A class Square that inherits from Rectangle
+        """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
