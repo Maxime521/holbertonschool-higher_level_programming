e4a0ca722429bf3be57792ef837e4b48feaecb9d
Maxime521 <9842@holbertonstudents.com>
2025-02-03 19:18:04 +0100

    I just finished the correction in the code

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -19,6 +19,7 @@ class BaseGeometry:
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
 
+
 class Rectangle(BaseGeometry):
     """
         A class Rectangle that inherits from BaseGeometry
