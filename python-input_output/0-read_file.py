b5875ff0ed916b05a0ea1b2b3e93e204ec67e2d4
Maxime521 <9842@holbertonstudents.com>
2025-02-10 22:17:14 +0100

    I just finished the task 2

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
+""" Module for read_file method """
+
 
 def read_file(filename=""):
-    """
-    Reads a text file (UTF-8 encoded) and prints its content to stdout.
-
-    Args:
-        filename (str): The path to the file to be read. Defaults to an empty string.
-    """
-    with open(filename, mode="r", encoding="utf-8") as file:
-        print(file.read(), end="")
+    """ function that reads a text file (UTF8) and prints it to stdout """
+    with open(filename, 'r', encoding="utf-8") as f:
+        print(f.read(), end='')
