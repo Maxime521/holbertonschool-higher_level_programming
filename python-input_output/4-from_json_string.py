7b53ef6479374052d3fc377717342fcf08021ba7
Maxime521 <9842@holbertonstudents.com>
2025-02-11 12:26:18 +0100

    I just organized the comments

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-"""
-This module defines a function from_json_string
-that returns an object represented by a JSON string.
-"""
-
-
+"""This module defines a function from_json_string"""
 import json
 
 
 def from_json_string(my_str):
+    """that returns an object represented by a JSON string."""
     return json.loads(my_str)
