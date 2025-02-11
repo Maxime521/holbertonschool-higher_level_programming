873a2375f1dcc886ca24fc868482dd1da959dc76
Maxime521 <9842@holbertonstudents.com>
2025-02-11 11:12:01 +0100

    I just added the new line

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -3,9 +3,8 @@
 This module defines a function to_json_string
 that returns the JSON representation of an object (string).
 """
-
-
 import json
+
 
 def to_json_string(my_obj):
     return json.dumps(my_obj)
