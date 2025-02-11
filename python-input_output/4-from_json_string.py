aecb190bc55d326ade1bbf1171f6a33afae7ac45
Maxime521 <9842@holbertonstudents.com>
2025-02-11 11:12:26 +0100

    new line added

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -7,5 +7,6 @@ that returns an object represented by a JSON string.
 
 import json
 
+
 def from_json_string(my_str):
     return json.loads(my_str)
--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -7,6 +7,7 @@ an Object to a text file, using a JSON representation.
 
 import json
 
+
 def save_to_json_file(my_obj, filename):
     with open(filename, "w", encoding="utf-8") as file:
         json.dumps(my_obj, file)
