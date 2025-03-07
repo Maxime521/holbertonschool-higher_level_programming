37991c8fba4de7efb9da57092d42a460f17ac72c
Maxime521 <9842@holbertonstudents.com>
2025-03-07 14:16:12 +0100

    I just finished the task 1 and 2

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+lists all states with a name starting with N (upper N)
+from the database hbtn_0e_0_usa.
+    """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    def list_states():
+        """ Lists all states from the database hbtn_0e_0_usa
+        """
+
+        db = MySQLdb.connect(
+            host="localhost",
+            user=sys.argv[1],
+            password=sys.argv[2],
+            db=sys.argv[3],
+            charset="utf8",
+            port=3306
+            )
+        cur = db.cursor()
+
+        cur.execute("SELECT * FROM states \
+                    WHERE BINARY name LIKE 'N%' ORDER BY id ASC")
+
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row)
+
+        cur.close()
+        db.close()
+
+    list_states()
