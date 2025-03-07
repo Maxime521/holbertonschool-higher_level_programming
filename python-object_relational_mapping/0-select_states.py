e2c613e08fcad6929bc6dcc3ac23063579759e79
Maxime521 <9842@holbertonstudents.com>
2025-03-07 11:21:38 +0100

    I just finished modifying the code locations

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,36 +1,20 @@
 #!/usr/bin/python3
-"""
-List all states from hbtn_0e_0_usa, sorted by states.id in
-ascending order, using MySQLdb, and accepting
-three arguments: username, password, and database name.
-    """
+"""a script that lists all states from the database"""
+
+
 import MySQLdb
 import sys
 
 
 if __name__ == "__main__":
-    def list_states():
-        """ Lists all states from the database hbtn_0e_0_usa
-        """
 
-        db = MySQLdb.connect(
-            host="localhost",
-            user=sys.argv[1],
-            password=sys.argv[2],
-            db=sys.argv[3],
-            charset="utf8",
-            port=3306
-            )
-        cur = db.cursor()
-
-        cur.execute('SELECT * FROM states ORDER BY id ASC')
-
-        rows = cur.fetchall()
-
-        for row in rows:
-            print(row)
-
-        cur.close()
-        db.close()
-
-    list_states()
+    conn = MySQLdb.connect(host="localhost", port=3306,
+                           user=sys.argv[1], passwd=sys.argv[2],
+                           db=sys.argv[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
